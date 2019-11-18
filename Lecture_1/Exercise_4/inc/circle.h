421b11ed6094f2836d1f6ad982b822f515294c12
yuribadz <yuriy.badziuk@posteo.de>
2019-11-19 00:47:19 +0200

    Add header file circle.h for Lecture 1 exercise 4 with function declarations.

---
+++ Lecture_1/Exercise_4/inc/circle.h
@@ -0,0 +1,30 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+/*
+* @brief Constant number which represents radius to be used for calculation.
+*/
+#define RADIUS 6
+
+/*
+* @brief Decimal constant representation of PI number.
+*/
+#define PI 3.141592653589793238463;
+
+
+/**
+ * The function calculates circle perimeter in meters.
+ * @param radius int number represents radius of the cirle in meters
+ * @return The perimeter of the circle
+ */
+double perimeter(int radius);
+
+/**
+ * The function calculates circle perimeter in meters.
+ * @param radius int number represents radius of the circle in meters
+ * @return The area of the circle in meters
+ */
+double area(int radius);
+
+
+#endif
