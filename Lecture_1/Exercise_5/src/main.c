89552d26ea539c2244024071e5368e494a67362d
yuribadz <yuriy.badziuk@posteo.de>
2019-11-19 00:53:28 +0200

    Move comment blocks function descriptions from main.c to header file factorial.h

--- Lecture_1/Exercise_5/src/main.c
+++ Lecture_1/Exercise_5/src/main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
+#include "factorial.h"
 
-#define NUMBER 10
-
-/**
- * The function factorial of the given number.
- * @param number Number used to calculate its factorial
- * @return Number which represents factorial of a given number
- */
 int factorial(int number)
 {
 	int factorial = 1;
