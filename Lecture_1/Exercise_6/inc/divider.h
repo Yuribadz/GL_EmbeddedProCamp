b1cea4a890f5bed1c4c081d18218984594c82fc1
yuribadz <yuriy.badziuk@posteo.de>
2019-11-19 00:48:51 +0200

    Add header file divider.h for Lecture 1 exercise 6 with function declarations.

---
+++ Lecture_1/Exercise_6/inc/divider.h
@@ -0,0 +1,10 @@
+#ifndef DIVIDER_H
+#define DIVIDER_H
+
+/**
+ * The function divides every number in range from 1 to 500 and prints it.
+ * @param divider double number represends divider for numbers in range
+ */
+void divide_print(double divider);
+
+#endif
