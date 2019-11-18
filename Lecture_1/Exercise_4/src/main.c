320e317fae368e9b616d1b7df08fbebff72e4b2f
yuribadz <yuriy.badziuk@posteo.de>
2019-11-19 00:53:00 +0200

    Move comment blocks function descriptions from main.c to header file circle.h

--- Lecture_1/Exercise_4/src/main.c
+++ Lecture_1/Exercise_4/src/main.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
+#include "circle.h"
 
-#define RADIUS 6
-#define PI 3.141592653589793238463;
-
-/**
- * The function calculates circle perimeter in meters.
- * @param radius int number represents radius of the cirle in meters
- * @return The perimeter of the circle
- */
-double perimeter(double radius)
+double perimeter(int radius)
 {
 	return radius * 2 * PI;
 }
 
-/**
- * The function calculates circle perimeter in meters.
- * @param radius int number represents radius of the circle in meters
- * @return The area of the circle in meters
- */
 double area(int radius)
 {
 	return radius * radius * PI;
