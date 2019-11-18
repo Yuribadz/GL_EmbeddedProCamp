5b7abd81d1e7dd2a1f190a90cbb0e9c3951d3e47
yuribadz <yuriy.badziuk@posteo.de>
2019-11-19 00:51:45 +0200

    Move comment blocks function descriptions from main.c to header file convert.h

--- Lecture_1/Exercise_2/src/main.c
+++ Lecture_1/Exercise_2/src/main.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
+#include "convert.h"
 
-#define RED_MASK 0xf80000
-#define RED_SHIFT 8
-#define GREEN_MASK 0xfc00
-#define GREEN_SHIFT 5
-#define BLUE_MASK 0xf8
-#define BLUE_SHIFT 3
-
-/**
- * The function converts pixel colors from RGB888 to RGB565 format.
- * @param pixel uint32_t number which represents red RGB888 pixel color
- * @return uint16_t number which represents RGB565 pixel color
- */
 uint16_t convert(uint32_t pixel)
 {
 	return (((pixel & RED_MASK) >> RED_SHIFT) +
@@ -23,6 +12,6 @@ uint16_t convert(uint32_t pixel)
 int main(int argc, char *argv[])
 {
 	uint32_t pixel = 0xffffff;
-	printf("%#04x = \n", convert(pixel));
+	printf("Converted RGB888 0xffffff to RGB565 %#04x\n", convert(pixel));
 	return 0;
 }
