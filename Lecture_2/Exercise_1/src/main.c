b821ae1bf3a81da39fbc3c99453815cab633da43
yuribadz <yuriy.badziuk@posteo.de>
2019-12-08 15:03:48 +0200

    Cleanup Lecture 2 Exercise 1 main.c file from unnecessary lines.

--- Lecture_2/Exercise_1/src/main.c
+++ Lecture_2/Exercise_1/src/main.c
@@ -26,7 +26,6 @@ int main(void)
 {
 	char input[50];
 	char output[50];
-	memset(output,0,strlen(output));
 	printf("Enter string : \n");
 	scanf("%s", input);
 	printf("You have entered : %s\n",input);
