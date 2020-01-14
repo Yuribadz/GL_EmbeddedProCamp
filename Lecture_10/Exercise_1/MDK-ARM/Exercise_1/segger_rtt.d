d04daaf34e72d07b9f4b5454da785e3bf0e28843
Yurii Badziuk <yuriy.badziuk@posteo.de>
2020-01-14 17:57:03 +0200

    Add solution for tracelyzer with static memory allocation

---
+++ Lecture_10/Exercise_1/MDK-ARM/Exercise_1/segger_rtt.d
@@ -0,0 +1,4 @@
+exercise_1\segger_rtt.o: ..\Src\SEGGER_RTT.c
+exercise_1\segger_rtt.o: ../Inc/SEGGER_RTT.h
+exercise_1\segger_rtt.o: ../Inc/SEGGER_RTT_Conf.h
+exercise_1\segger_rtt.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
